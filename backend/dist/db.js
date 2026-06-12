1f496533a8423cdccd7446b359768f300ee16281
ksuraj2002 <sk2412815@gmail.com>
2026-06-12 13:58:12 +0530

    removed compiled dist folder from repository

--- backend/dist/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-let connectionPromise = null;
-export async function connectDb(uri) {
-    if (!connectionPromise) {
-        connectionPromise = mongoose.connect(uri);
-    }
-    return connectionPromise;
-}
